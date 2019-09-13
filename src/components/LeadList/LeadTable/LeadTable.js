96031f9636a3a1eccdcee9061ba91b1e27f57e93
MaxBocharnikov <Bocharnikov3009@gmail.com>
2019-09-13 15:18:18 +0700

    add some styles

--- src/components/LeadList/LeadTable/LeadTable.js
+++ src/components/LeadList/LeadTable/LeadTable.js
@@ -7,6 +7,8 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
+import './LeadTable.css';
+
 const useStyles = makeStyles(theme => ({
     root: {
         width: '100%',
